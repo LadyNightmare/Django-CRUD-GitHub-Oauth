3a47d2f00d7f6d28cb2926a5f5c476e4cd4fa2cb
LadyNightmare <ladynightmare2@outlook.com>
2021-10-20 20:42:32 +0200

    :card_file_box: Modify DB models

--- interviewproject/personalinfo/models.py
+++ interviewproject/personalinfo/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 TITLES = (
-    ('Mr.','Mr.'),
+    ('Mr.', 'Mr.'),
     ('Mrs.', 'Mrs.'),
-    ('Ms.','Ms.')
+    ('Ms.', 'Ms.')
 )
+
 
 # Create your models here.
 class Address(models.Model):
     street_line_one = models.CharField(max_length=100)
-    street_line_two = models.CharField(max_length=100)
+    street_line_two = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=50)
     province = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=10)
+    #active = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.street_line_one} {self.street_line_two}, {self.city}, {self.province}, {self.country}, " \
+               f"ZIP CODE: {self.zip_code}"
+
 
 class PersonalInfo(models.Model):
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
@@ -22,4 +28,7 @@ class PersonalInfo(models.Model):
     title = models.CharField(max_length=4, choices=TITLES, default='Ms.')
     birth_date = models.DateField()
     nationality = models.CharField(max_length=50)
-    phone_number = models.IntegerField(default=0)
+    phone_number = models.IntegerField()
+    def __str__(self):
+        return f"{self.title} {self.name} {self.surname}, {self.address}, {self.phone_number}, {self.nationality}, " \
+               f"ZIP CODE: {self.birth_date}"
